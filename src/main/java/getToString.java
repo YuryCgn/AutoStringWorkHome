f0ecd0f8fc99cbcce50c38d12532359cc8d92017
YuryCgn <61134191+YuryCgn@users.noreply.github.com>
2022-04-22 18:17:13 +0300

    Update getToString.java

--- src/main/java/getToString.java
+++ src/main/java/getToString.java
@@ -1,10 +1,13 @@
 public class getToString {
 
     public static void main(String[] args) {
-
+        
     }
 
     public static int add(int num) {
+        // Для чего здесь параметр num ?
+        // Ты же его нигде не используешь
+        
         String str1 = "This is exercise 1";
         String str2 = "This is Exercise 1";
 
